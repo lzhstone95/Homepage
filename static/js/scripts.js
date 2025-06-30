1a92c5b4709be34e1f702925d1587ddf4a7d31c3
ZhonghuiLi <lzhstone95@gmail.com>
2025-06-30 15:49:00 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'news', 'publications', 'projects', 'services', 'resume']
+const section_names = ['home', 'news', 'teaching', 'publications', 'projects', 'services', 'resume']
 
 
 window.addEventListener('DOMContentLoaded', event => {
